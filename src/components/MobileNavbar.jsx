3398f09be726b612c9eb93d35f6d92793354ce59
Atheeb <atheebvalued@gmail.com>
2024-07-05 09:14:49 +0530

    a

--- src/components/MobileNavbar.jsx
+++ src/components/MobileNavbar.jsx
@@ -4,7 +4,7 @@ import { AiOutlineBars } from 'react-icons/ai';
 const MobileNavbar = ({openNav, setOpenNav}) => {
   return (
     <nav className='flex items-center justify-between p-4 md:hidden fixed w-full backdrop-blur-2xl z-50'>
-        <div className='h-[50px] w-[150px]'>
+        <div className='h-[50px] w-[150px] text-[#fff]'>
         <strong>   <h1>CPS Prime 3.0</h1></strong>
         </div>
         <div>
