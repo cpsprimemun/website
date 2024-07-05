a6bebe9529189bbf518123037bd77180d8419d4a
Atheeb <atheebvalued@gmail.com>
2024-07-05 09:12:25 +0530

    W

--- src/components/MobileNavbar.jsx
+++ src/components/MobileNavbar.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import Logo from '../assets/logo.png';
 import { AiOutlineBars } from 'react-icons/ai';
 
 const MobileNavbar = ({openNav, setOpenNav}) => {
   return (
     <nav className='flex items-center justify-between p-4 md:hidden fixed w-full backdrop-blur-2xl z-50'>
         <div className='h-[50px] w-[150px]'>
-            <img src={Logo} alt="allotrix" />
+        <strong>   <h1>CPS Prime 3.0</h1></strong>
         </div>
         <div>
             <button className='text-4xl text-[white]' onClick={() => setOpenNav(!openNav)}>
