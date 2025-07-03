7b0adf2c617325a8fd8393f3ccf90b35508846c0
cpsprimemun <cpsprimemun@gmail.com>
2025-07-03 22:37:08 +0530

    Secretary change

--- src/components/Secgen.jsx
+++ src/components/Secgen.jsx
@@ -7,18 +7,21 @@ const Secgen = () => {
     <div className='border-2 border-cps-grey p-4 rounded-xl bg-white flex flex-col-reverse md:flex-row gap-4'>
             <div className='md:w-[70%] w-full flex flex-col justify-evenly'>
                 <h1  className='text-2xl'>
-                    Harini Sankar
+                    
                 </h1>
-                <h3 className='font-bold text-cps-grey text-3xl'>Secretary General</h3>
+                
+                
+                    Prithvi Easwaramoorthy | Shruti Malaar Damother | Harivathsan HK | Praneeth V
+                <h3 className='font-bold text-cps-grey text-3xl'>Secretariats</h3>
                 <p className='font-regular text-base opacity-35	'> Greetings!<br></br><br></br>
 
-With great honour and pride, I welcome all of you to the third edition of Chennai Public School’s Prime Model United Nation. As the Secretary General of this edition, I assure you that CPS PRIME MUN is the perfect place to hone your skills in formal debate and to make connections that last a lifetime. Participating in MUNs is not only about the awards or the recognition you get but also about the experience and exposure you gain. Our well equipped Executive Board members have faced all the trials and tribulations of being a delegate as they stand on the other side of the table to judge you. An event of such a large scale would also aid you in socialising with others, as you begin to open your mind to all the perspectives that other delegates hold, by coming together to solve issues that plague our society. 
+“Debate is a constructive conversation, because no one holds a monopoly on being right”
+With this belief at core Chennai Public School Anna Nagar  is proud to welcome you all to the 4 Th edition of CPS PRIME Model United Nations. 
+Be part of Chennai’s best mun where memories are made and leaders are shaped. An arena to showcase your diplomatic skills , debating mindset and spokesman ship . CPS PRIME MUN is more than just a conference, it is an opportunity to exhibit your critical thinking and leadership acumen. Engage in thought provoking committees that are led by an exceptional executive board where every word spoken adds to your growth .
+The knowledge demanding agendas will spark new insights and fresh perspectives into your minds . CPS has worked tirelessly to make sure that PRIME enables everyone’s thoughts to be heard loud and clear through nurturing global awareness and igniting passion for policy making,which will unfold in the intellectual committees . 
+With that We would like all of you to register for CPS prime taking place on 1st and 2nd August . If you have any quesries feel free to reach out to us and as the secretariat we assure you that this event will not only be a learning experience but also a journey that you will cherish even after the final gavel drops.
 
- With over 10 committees encompassing varying geopolitical and national issues, CPS PRIME MUN stands at the foreground in connecting you to the world. As a delegate in the conference, you’ll be a part of one of Chennai’s Biggest MUNs with debate of utmost quality and its very own Socials.
-
- I kindly request all of you to register yourself for CPS PRIME MUN’24 happening on July 19th and 20th. I’m looking forward to your esteemed presence to grace our event. Don’t hesitate to reach out to us anytime for assistance. 
-
- Thank you 
+Thank You
  
                 </p>
                 <br></br>
